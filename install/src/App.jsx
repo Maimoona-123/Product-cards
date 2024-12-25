7499ff7aa8256daca975420796fd7d7fd99b8813
maimoona <maimoonaaltaf7@gmail.com>
2024-12-25 17:22:09 +0500

    product-list

---
+++ install/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Card from './Components/Card'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='heading'>Product List</h1>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
+
